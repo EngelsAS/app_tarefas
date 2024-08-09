50f643d004ea10b75accf9f3e3b8ab0b92f68654
EngelsAS <engelsantero@gmail.com>
2024-08-08 23:22:26 -0300

    Adicionando arquivos ao repositorio

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import styles from "./styles.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+          <Link href={"/"}>
+            <h1 className={styles.logoNav}>
+              Tarefas<span>+</span>
+            </h1>
+          </Link>
+          <Link href={"/dashboard"} className={styles.linkMyPanel}>
+            Meu Painel
+          </Link>
+        </nav>
+
+        <button className={styles.loginButton}>Acessar</button>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
