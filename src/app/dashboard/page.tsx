a454719d1034ede51073d994d522448d5800ab6f
EngelsAS <engelsantero@gmail.com>
2024-08-11 15:03:16 -0300

    feat: formulario de registro de nova tarefa na pagina dasboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,45 @@
-const Dashboard = () => {
-  return <div>page</div>;
+import { Metadata } from "next";
+import { getAuthSession } from "../lib/auth";
+import { redirect } from "next/navigation";
+import styles from "./styles.module.css";
+import TextArea from "@/components/TextArea";
+
+export const metadata: Metadata = {
+  title: "Meu Painel de Tarefas",
+};
+
+const redirectIfNotAuthenticated = async () => {
+  const session = await getAuthSession();
+
+  if (!session) redirect("/");
+};
+
+const Dashboard = async () => {
+  await redirectIfNotAuthenticated();
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
+
+            <form>
+              <TextArea placeholder="Digite qual sua tarefa..." />
+              <div className={styles.checkboxContainer}>
+                <input type="checkbox" className={styles.checkbox} />
+                <label>Deixar tarefa pública?</label>
+              </div>
+
+              <button type="submit" className={styles.button}>
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Dashboard;
