e8629d8dbc4f2ec21c9a3946e986b299c6733b73
EngelsAS <engelsantero@gmail.com>
2024-09-18 17:33:04 -0300

    fix: route.ts and auth.ts

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,5 +1,15 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { getServerSession, NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.JWT_SECRET as string,
+};
 
 export async function getAuthSession() {
   return await getServerSession(authOptions);
