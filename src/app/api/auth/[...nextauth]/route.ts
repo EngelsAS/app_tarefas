1989e98dfd3b65a2f537398bd6bbc50923f172c0
EngelsAS <engelsantero@gmail.com>
2024-09-18 17:39:26 -0300

    fix: route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import { authOptions } from "@/app/lib/auth";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
